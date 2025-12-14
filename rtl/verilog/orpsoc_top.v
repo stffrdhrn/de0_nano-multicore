346a18e40e6e14827fadcbed5041cdad145e5f73
Stafford Horne <shorne@gmail.com>
2025-12-14 08:08:05 +0000

    sor: Fix GPIO0, remove 8-bit converter
    For some reason we had this 8-bit converter for gpio0, it is not present
    on the other de0 nano single core SoC.  With it gpio0 was not working
    and linux would crash when writing to the GPIO.  Removing it fixes the
    issues so we just remove it.

--- rtl/verilog/orpsoc_top.v
+++ rtl/verilog/orpsoc_top.v
@@ -1269,18 +1269,6 @@ wire [7:0]	gpio0_in;
 wire [7:0]	gpio0_out;
 wire [7:0]	gpio0_dir;
 
-wire [31:0]	wb8_m2s_gpio0_adr;
-wire [1:0]	wb8_m2s_gpio0_bte;
-wire [2:0]	wb8_m2s_gpio0_cti;
-wire		wb8_m2s_gpio0_cyc;
-wire [7:0]	wb8_m2s_gpio0_dat;
-wire		wb8_m2s_gpio0_stb;
-wire		wb8_m2s_gpio0_we;
-wire [7:0] 	wb8_s2m_gpio0_dat;
-wire		wb8_s2m_gpio0_ack;
-wire		wb8_s2m_gpio0_err;
-wire		wb8_s2m_gpio0_rty;
-
 // Tristate logic for IO
 // 0 = input, 1 = output
 genvar                    i;
@@ -1297,49 +1285,19 @@ gpio gpio0 (
 	.gpio_o		(gpio0_out),
 	.gpio_dir_o	(gpio0_dir),
 	// Wishbone slave interface
-	.wb_adr_i	(wb8_m2s_gpio0_adr[0]),
-	.wb_dat_i	(wb8_m2s_gpio0_dat),
-	.wb_we_i	(wb8_m2s_gpio0_we),
-	.wb_cyc_i	(wb8_m2s_gpio0_cyc),
-	.wb_stb_i	(wb8_m2s_gpio0_stb),
-	.wb_cti_i	(wb8_m2s_gpio0_cti),
-	.wb_bte_i	(wb8_m2s_gpio0_bte),
-	.wb_dat_o	(wb8_s2m_gpio0_dat),
-	.wb_ack_o	(wb8_s2m_gpio0_ack),
-	.wb_err_o	(wb8_s2m_gpio0_err),
-	.wb_rty_o	(wb8_s2m_gpio0_rty),
+	.wb_adr_i	(wb_m2s_gpio0_adr[0]),
+	.wb_dat_i	(wb_m2s_gpio0_dat),
+	.wb_we_i	(wb_m2s_gpio0_we),
+	.wb_cyc_i	(wb_m2s_gpio0_cyc),
+	.wb_stb_i	(wb_m2s_gpio0_stb),
+	.wb_cti_i	(wb_m2s_gpio0_cti),
+	.wb_bte_i	(wb_m2s_gpio0_bte),
+	.wb_dat_o	(wb_s2m_gpio0_dat),
+	.wb_ack_o	(wb_s2m_gpio0_ack),
+	.wb_err_o	(wb_s2m_gpio0_err),
 
 	.wb_clk		(wb_clk),
 	.wb_rst		(wb_rst)
-);
-
-// 32-bit to 8-bit wishbone bus resize
-wb_data_resize wb_data_resize_gpio0 (
-	// Wishbone Master interface
-	.wbm_adr_i	(wb_m2s_gpio0_adr),
-	.wbm_dat_i	(wb_m2s_gpio0_dat),
-	.wbm_sel_i	(wb_m2s_gpio0_sel),
-	.wbm_we_i	(wb_m2s_gpio0_we ),
-	.wbm_cyc_i	(wb_m2s_gpio0_cyc),
-	.wbm_stb_i	(wb_m2s_gpio0_stb),
-	.wbm_cti_i	(wb_m2s_gpio0_cti),
-	.wbm_bte_i	(wb_m2s_gpio0_bte),
-	.wbm_dat_o	(wb_s2m_gpio0_dat),
-	.wbm_ack_o	(wb_s2m_gpio0_ack),
-	.wbm_err_o	(wb_s2m_gpio0_err),
-	.wbm_rty_o	(wb_s2m_gpio0_rty),
-	// Wishbone Slave interface
-	.wbs_adr_o	(wb8_m2s_gpio0_adr),
-	.wbs_dat_o	(wb8_m2s_gpio0_dat),
-	.wbs_we_o	(wb8_m2s_gpio0_we ),
-	.wbs_cyc_o	(wb8_m2s_gpio0_cyc),
-	.wbs_stb_o	(wb8_m2s_gpio0_stb),
-	.wbs_cti_o	(wb8_m2s_gpio0_cti),
-	.wbs_bte_o	(wb8_m2s_gpio0_bte),
-	.wbs_dat_i	(wb8_s2m_gpio0_dat),
-	.wbs_ack_i	(wb8_s2m_gpio0_ack),
-	.wbs_err_i	(wb8_s2m_gpio0_err),
-	.wbs_rty_i	(wb8_s2m_gpio0_rty)
 );
 
 wire [0:1] ipi_irq;
